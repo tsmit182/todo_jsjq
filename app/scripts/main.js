eed5d6dc925fcbd049795fa96febe591dcefaa76
Taylor Smith <tsmit182@gmail.com>
2014-08-25 18:37:59 -0400

    adding input to field; strikes items collectively instead of individually on click

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,25 @@
-$('.itemSubmit').click(function() {
-	console.log('clicked')
+//consider defining an empty global variable, should you need it:
+// var App = {};
 
-})
+//click `.itemSubmit` to run following code block
+$('.itemSubmit').click(function() {
+	//inputVal gets user-input value of `.itemInput`
+	var inputVal = $('.itemInput').val()
+	console.log(inputVal);
+	$('.itemField').append('<div class="todoItem active">' + inputVal + '</div>');
+		var item = $('.todoItem');
+
+		item.click(function() {
+			console.log('clicked')
+			item.addClass('strikethrough')
+			item.removeClass('active');
+		});
+
+
+});
+
+$('h2').click(function(){
+	console.log('clicked')
+	$('h2').addClass('strikethrough');
+});
+
