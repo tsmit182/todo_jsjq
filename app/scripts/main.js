61a907276074cb2d348036d1603e6e87607b7155
Taylor Smith <tsmit182@gmail.com>
2014-08-26 16:15:31 -0400

    different things i've tried commented out. still not selecting only one element

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,30 +6,85 @@ $('.itemSubmit').click(function() {
 	//inputVal gets user-input value of `.itemInput`
 	var inputVal = $('.itemInput').val()
 	console.log(inputVal);
-	$('.itemField').append('<div class="todoItem active">' + inputVal + '</div>');
-		var item = $('.todoItem');
+	$('.itemField').append('<div class="todoCont"><div class="todoItem">' + inputVal + '</div></div>');
+		
 
 
-		item.click(function() {
-			if ($(this).hasClass('active')){
-				$(this).removeClass('active');
-				$(this).addClass('strikethrough');
+	$('.todoItem').click(function() {
+		$(this).toggleClass('strikethrough');
+		$(this).remove();
+	});
 
-			// } else if ($(this).hasClass('strikethrough')){
-			// 	console.log('strikethrough click!');
-			// 	$(this).removeClass('strikethrough');
-			// 	$(this).addClass('active');
+// var item = $('.todoItem');
+		// item.click(function() {
+		// 	if ($(item).hasClass('active')){
+		// 		$(this).removeClass('active');
+		// 		$(this).addClass('strikethrough');
+
+		// 	} else if ($(this).hasClass('strikethrough')){
+		// 		console.log('strikethrough click!');
+		// 		$(this).removeClass('strikethrough');
+		// 		$(this).addClass('active');
 				
-			// } else
-			} else {
-				console.log('hello')
-				$(this).removeClass('strikethrough');
-								$(this).addClass('active');
-			}
-		});
+		// 	// } else
+		// 	} else {
+		// 		console.log('hello')
+		// 		$(this).removeClass('strikethrough');
+		// 		$(this).addClass('active');
+		// 	}
+		// });
 
+
+
+		// item.on('click', function(e) {
+		// 	// $(this).toggleClass('active strikethrough');
+		// 	$(this).setAttribute('todoItem', 'strikethrough');
+		// });
+
+
+
+	// $('.active').click(function(){
+	// 	console.log('active targeted');
+	// })
+	// $('.strikethrough').click(function(){
+	// 	console.log(this+'targeted');
+	// })
+
+
+
+
+
+		// $('.todoItem.active').click(function(){
+		// 	console.log('active targeted');
+		// 	$(this).addClass('strikethrough').removeClass('active');
+		// 	$('.todoItem.strikethrough').click(function(){
+		// 	console.log('strikethrough targeted')
+		// 	$(this).addClass('active').removeClass('strikethrough');
+		// });});
+
+
+		// $('.todoItem').click(function(){
+		// 	if ($(this).hasClass('active')){
+		// 		$(this).addClass('strikethrough');
+		// 	} else if ($(this).hasClass('strikethrough')) {
+		// 		$(this).removeClass('strikethrough');
+		// 	}
+		// });
+
+
+		
+
+		//
+	
 
 });
+
+
+
+
+
+
+
 
 $('h2').click(function(){
 	console.log('clicked')
